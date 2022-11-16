106c98623c707ca1c300ead6fbec0cca6338d652
Jitske <jitske.arends@experius.nl>
2022-11-16 11:36:35 +0100

    [FEATURE][TEO-90]-recipes dynamic view created

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -1,11 +1,30 @@
 <x-layout>
-    <x-slot name="content">
-        @foreach($recipes as $recipe)
-            <div>
-            <a href="/recipes/{{ $recipe->slug }}" >{!! $recipe->title !!}</a>
-            <img src="{{ $recipe->image }}" alt="Plate" width="200" />
-            </div>
-        @endforeach
-    </x-slot>
+    @include('_recipes-header');
 
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        @if ($recipes->count())
+
+            <div class="lg:grid lg:grid-cols-2">
+
+                <x-recipe-featured-card :recipe="$recipes[0]"/>
+                <x-cocktail-featured-card />
+
+            </div>
+
+            @if ($recipes->count() > 1)
+                <div class="lg:grid lg:grid-cols-3">
+                    @foreach($recipes->skip(1) as $recipe)
+                        <x-recipe-card :recipe="$recipe"/>
+                    @endforeach
+                </div>
+            @endif
+
+        @else
+            <p>Er zijn nog geen recepten beschikbaar, we zien je graag later terug.</p>
+        @endif
+
+    </main>
 </x-layout>
+
+
