fcf3820f25a2e44927414e04f85741b3ac4642b6
Jitske <jitske.arends@experius.nl>
2022-11-21 11:05:31 +0100

    [FEATURE][TEO-73]-constant for api url

--- app/Service/CocktailApi.php
+++ app/Service/CocktailApi.php
@@ -6,37 +6,37 @@ use Illuminate\Support\Facades\Http;
 
 class CocktailApi
 {
-    protected $url = 'http://www.thecocktaildb.com/api/json/v1/1/';
+   protected const URL = 'http://www.thecocktaildb.com/api/json/v1/1/';
 
     public function getByName($name)
     {
-        Http::get($this->url . 'search.php', [
+        Http::get($this::URL . 'search.php', [
             's' => $name
         ])->json($key = null);
     }
 
     public function getById($id)
     {
-        Http::get($this->url . 'lookup.php', [
+        Http::get($this::URL . 'lookup.php', [
             'i' => $id
         ])->json($key = null);
     }
 
     public function getRandom()
     {
-        return Http::get($this->url . 'random.php')->json($key = null);
+        return Http::get($this::URL . 'random.php')->json($key = null);
     }
 
     public function searchByIngredient($ingredient)
     {
-        return Http::get($this->url . 'filter.php', [
+        return Http::get($this::URL . 'filter.php', [
             'i' => $ingredient
         ])->json($key = null);
     }
 
     public function alcoholicOrNonAlcoholic($choice)
     {
-        return Http::get($this->url . 'filter.php', [
+        return Http::get($this::URL . 'filter.php', [
             'a' => $choice
         ])->json($key = null);
     }
